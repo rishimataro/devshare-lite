058cd1d1f680fbf186d8fc72990f05334ecbe592
Ngoc Thao <ngocthaodana@gmail.com>
2025-07-07 22:05:12 +0700

    feat(config): add transpilePackages and experimental optimizePackageImports for Ant Design

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: [
+    '@ant-design/nextjs-registry',
+    '@ant-design/colors',
+    '@ant-design/icons',
+    '@ant-design/icons-svg',
+    'antd',
+    'rc-util',
+    'rc-pagination',
+    'rc-picker',
+    'rc-tree',
+    'rc-table',
+  ],
+  experimental: {
+    optimizePackageImports: ['antd'],
+  },
 };
 
 export default nextConfig;
